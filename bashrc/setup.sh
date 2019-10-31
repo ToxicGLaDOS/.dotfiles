32d354c068efe377c4b3c8bf7bebdf4c67c90a1a
Jeff <jeffery.smith@pearson.com>
2019-10-31 16:55:41 -0500

    Mirrored layout for files as if they're in the home directory

--- bashrc/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm ~/.bashrc
-rm ~/.bash_aliases
-currentdir=$(pwd);
-ln ${currentdir}/.bashrc ~/.bashrc
-ln ${currentdir}/.bash_aliases ~/.bash_aliases
-echo "$(echo '@hourly' $currentdir'/sync.sh' ; crontab -l)" | crontab -
