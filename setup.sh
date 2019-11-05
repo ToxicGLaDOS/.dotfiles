c6c5d95ce839b6116726c39c2a1e02b50ebed562
Jeff Smith <toxicglados@gmail.com>
2019-11-04 22:43:38 -0600

    Corrected i3blocks directory and added some ignore functionality

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -x
-script_dir=$(dirname "$0")
+script_dir=$(realpath $(dirname "$0"))
 cd $script_dir
 
 home_dir="$HOME/test"
 
-top_levels=$(ls -A)
+top_levels=$(find . | grep -vFf .ignore)
 
-for top_level in $top_levels; do
-    ln -s $script_dir/$top_level  $home_dir/$top_level
+for top_level in $top_levels; do     
+    #if [ -f "$home_dir/$top_level" ]; then
+    #    rm $home_dir/$top_level
+    #fi
+    ln -s $script_dir/$top_level  $home_dir/$top_level/
 done
 
