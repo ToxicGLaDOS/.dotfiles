f2ad6bd5a4cc4d08ab933d4b107db75c6f00bb41
Jeff <jeffery.smith@pearson.com>
2019-11-12 10:49:08 -0600

    Update setup.sh and .gitignore

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ script_dir=$(realpath $(dirname "$0"))
 cd $script_dir
 
 backup_dir="$script_dir/backup"
-home_dir="$script_dir/test"
+home_dir="$HOME"
 
 paths=$(find . ! -path . | grep -vFf .ignore | sort)
 
@@ -17,28 +17,13 @@ else
     echo "Force mode is off. Use flag '-f' to actually make changes"
 fi
 
+if [ -d $backup_dir ]; then
+    rm -r $backup_dir
+fi
+
 if [ ! -d $backup_dir ]; then
     mkdir $backup_dir
 fi
-find $backup_dir -mindepth 1 -delete
-
-#for path in $paths; do
-#    if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
-#        DIFF=$(diff -rq $home_dir/$path $script_dir/$path)
-#
-#        # If they're different
-#        if [ "$DIFF" != "" ]; then
-#            # Then just copy the file to the right directory in the backup dir
-#            if [ -f $home_dir/$path ]; then
-#                cp $home_dir/$path $backup_dir/$path
-#                                 
-#            # Then make a mirror directory in the backup
-#            elif [ -d $home_dir/$path ]; then
-#                mkdir $backup_dir/$path
-#            fi
-#        fi
-#    fi
-#done
 
 top_levels=$(find . -maxdepth 1 ! -path . | grep -vFf .ignore | sort)
 
@@ -49,26 +34,38 @@ for path in $top_levels; do
             cp -r $home_dir/$path $backup_dir/$path
             echo "remove $home_dir/$path"
             rm -r $home_dir/$path
+        elif [ -L $home_dir/$path ]; then
+            echo "delete broken link at $home_dir/$path"
+            rm $home_dir/$path 
         fi
         echo "link $home_dir/$path to $script_dir/$path"
         ln -s $script_dir/$path $home_dir/$path
         
-        if [ -f $backup_dir/$path ] || [ -d $backup_dir/$path ]; then
-
-            echo "copy -T $backup_dir/$path into $home_dir/$path"
+        if [ -f $backup_dir/$path ]; then
+            echo "copy -T $backup_dir/$path (file) into $home_dir/$path"
             cp -rT $backup_dir/$path $home_dir/$path
+
+        elif [ -d $backup_dir/$path ] && [ ! -L $backup_dir/$path ]; then
+            # The trailing slash at the end of the second arg here is important!
+            echo "copy -T $backup_dir/$path (directory) into $home_dir/$path/"
+            cp -rT $backup_dir/$path $home_dir/$path/
         fi
     else
         if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
             echo "copy $home_dir/$path into $backup_dir/$path"
             echo "remove $home_dir/$path"
+        elif [ -L $home_dir/$path ]; then
+            echo "delete broken link at $home_dir/$path"
         fi
         echo "link $home_dir/$path to $script_dir/$path"
+        
+        if [ -f $backup_dir/$path ]; then
+            echo "copy -T $backup_dir/$path (file) into $home_dir/$path"
 
-        if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
-            echo "copy -rT $backup_dir/$path into $home_dir/$path"
+        elif [ -d $backup_dir/$path ]; then
+            # The trailing slash at the end of the second arg here is important!
+            echo "copy -T $backup_dir/$path (directory) into $home_dir/$path/"
         fi
-
     fi
 done
 
