54fc8444661fb8356e589950626bed092bc2acbf
Jeff Smith <toxicglados@gmail.com>
2019-11-11 23:25:46 -0600

    Testing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 script_dir=$(realpath $(dirname "$0"))
 cd $script_dir
 
@@ -10,46 +11,64 @@ paths=$(find . ! -path . | grep -vFf .ignore | sort)
 
 if [ "$1" == "-f" ]; then
     force=true
+    echo "Force mode is on! Changes will happen!"
 else
     force=false
+    echo "Force mode is off. Use flag '-f' to actually make changes"
 fi
 
 if [ ! -d $backup_dir ]; then
     mkdir $backup_dir
 fi
-echo $(find $backup_dir -mindepth 1)
+find $backup_dir -mindepth 1 -delete
 
-for path in $paths; do
-    if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
-        DIFF=$(diff -rq $home_dir/$path $script_dir/$path)
+#for path in $paths; do
+#    if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
+#        DIFF=$(diff -rq $home_dir/$path $script_dir/$path)
+#
+#        # If they're different
+#        if [ "$DIFF" != "" ]; then
+#            # Then just copy the file to the right directory in the backup dir
+#            if [ -f $home_dir/$path ]; then
+#                cp $home_dir/$path $backup_dir/$path
+#                                 
+#            # Then make a mirror directory in the backup
+#            elif [ -d $home_dir/$path ]; then
+#                mkdir $backup_dir/$path
+#            fi
+#        fi
+#    fi
+#done
 
-        # If they're different
-        if [ "$DIFF" != "" ]; then
-            # Then just copy the file to the right directory in the backup dir
-            if [ -f $home_dir/$path ]; then
-                cp $home_dir/$path $backup_dir/$path
-                if [ "$force" = true ]; then
-                    echo "BLAH"
-                else
-                    echo "would remove $home_dir/$path"
-                fi
-                 
-            # Then make a mirror directory in the backup
-            elif [ -d $home_dir/$path ]; then
-                mkdir $backup_dir/$path
-            fi
+top_levels=$(find . -maxdepth 1 ! -path . | grep -vFf .ignore | sort)
+
+for path in $top_levels; do
+    if [ "$force" = true ]; then
+        if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
+            echo "copy $home_dir/$path into $backup_dir/$path"
+            cp -r $home_dir/$path $backup_dir/$path
+            echo "remove $home_dir/$path"
+            rm -r $home_dir/$path
         fi
+        echo "link $home_dir/$path to $script_dir/$path"
+        ln -s $script_dir/$path $home_dir/$path
+        
+        if [ -f $backup_dir/$path ] || [ -d $backup_dir/$path ]; then
+
+            echo "copy -T $backup_dir/$path into $home_dir/$path"
+            cp -rT $backup_dir/$path $home_dir/$path
+        fi
+    else
+        if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
+            echo "copy $home_dir/$path into $backup_dir/$path"
+            echo "remove $home_dir/$path"
+        fi
+        echo "link $home_dir/$path to $script_dir/$path"
+
+        if [ -f $home_dir/$path ] || [ -d $home_dir/$path ]; then
+            echo "copy -rT $backup_dir/$path into $home_dir/$path"
+        fi
+
     fi
 done
-
-
-
-
-
-#for file in $files; do
-#    if [ -f "$home_dir/$file" ] || [ -d "$home_dir/$file" ]; then
-#        rm -r $home_dir/$file
-#    fi
-#    ln -s $script_dir/$file  $home_dir/$file
-#done
 
